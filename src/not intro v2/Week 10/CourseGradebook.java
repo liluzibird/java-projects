2cc27989439c4046e16ce3a6ba648e3ab4cf27db
liluzibird <57073805+liluzibird@users.noreply.github.com>
2024-04-21 17:33:06 -0700

    upload completed project

--- src/not intro v2/Week 10/CourseGradebook.java
+++ src/not intro v2/Week 10/CourseGradebook.java
@@ -1,41 +1,56 @@
 import java.util.*;
 
 public class CourseGradebook extends Gradebook {
-   // Declare any protected fields here (change placeholder field below)
-	protected int field;
-	
-	@Override
-	public HashMap<Integer, Double> getAssignmentScores(String assignmentName) {
-		// Your code here (remove placeholder line below)
-		return new HashMap<Integer, Double>();
-	}
-	
-	@Override
-	public double getScore(String assignmentName, Integer studentID) {
-		// Your code here
-		return Double.NaN;
-	}
-	
-	@Override
-	public ArrayList<String> getSortedAssignmentNames() {
-		// Your code here (remove placeholder line below)
-		return new ArrayList<String>();
-	}
-	
-	@Override
-	public ArrayList<Integer> getSortedStudentIDs() {
-		// Your code here (remove placeholder line below)
-		return new ArrayList<Integer>();
-	}
+    protected HashMap<String, HashMap<Integer, Double>> gradebook;
 
-	@Override
-	public HashMap<String, Double> getStudentScores(Integer studentID) {
-		// Your code here (remove placeholder line below)
-		return new HashMap<String, Double>();
-	}
-	
-	@Override
-	public void setScore(String assignmentName, Integer studentID, Double score) {
-		// Your code here
-	}
+    public CourseGradebook() {
+        gradebook = new HashMap<>();
+    }
+
+    @Override
+    public HashMap<Integer, Double> getAssignmentScores(String assignmentName) {
+        return gradebook.getOrDefault(assignmentName, new HashMap<>());
+    }
+
+    @Override
+    public double getScore(String assignmentName, Integer studentID) {
+        return gradebook.getOrDefault(assignmentName, new HashMap<>()).getOrDefault(studentID, Double.NaN);
+    }
+
+    @Override
+    public ArrayList<String> getSortedAssignmentNames() {
+        ArrayList<String> assignmentNames = new ArrayList<>(gradebook.keySet());
+        Collections.sort(assignmentNames);
+        return assignmentNames;
+    }
+
+    @Override
+    public ArrayList<Integer> getSortedStudentIDs() {
+        HashSet<Integer> studentIDs = new HashSet<>();
+        for (HashMap<Integer, Double> scores : gradebook.values()) {
+            studentIDs.addAll(scores.keySet());
+        }
+        ArrayList<Integer> sortedStudentIDs = new ArrayList<>(studentIDs);
+        Collections.sort(sortedStudentIDs);
+        return sortedStudentIDs;
+    }
+
+    @Override
+    public HashMap<String, Double> getStudentScores(Integer studentID) {
+        HashMap<String, Double> studentScores = new HashMap<>();
+        for (Map.Entry<String, HashMap<Integer, Double>> entry : gradebook.entrySet()) {
+            if (entry.getValue().containsKey(studentID)) {
+                studentScores.put(entry.getKey(), entry.getValue().get(studentID));
+            }
+        }
+        return studentScores;
+    }
+
+    @Override
+    public void setScore(String assignmentName, Integer studentID, Double score) {
+        if (!gradebook.containsKey(assignmentName)) {
+            gradebook.put(assignmentName, new HashMap<>());
+        }
+        gradebook.get(assignmentName).put(studentID, score);
+    }
 }
